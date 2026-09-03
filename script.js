be26f8c16173c8c96affbe098f21adb81cddbe41
VIJJWAL <vijjwalkaushik22@gmail.com>
2026-09-03 23:29:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 const edu = document.getElementById("edu");
 const skills = document.getElementById("skills");
-const project = document.getElementById("project");
+const exp = document.getElementById("exp");
 const activities = document.getElementById("activities");
 const certification = document.getElementById("certification");
 const home = document.getElementById("home");
 
 const eduBtn = document.getElementById("edu-btn");
 const skillsBtn = document.getElementById("skills-btn");
-const projectBtn = document.getElementById("project-btn");
+const expBtn = document.getElementById("exp-btn");
 const activitiesBtn = document.getElementById("activities-btn");
 const certificationBtn = document.getElementById("certification-btn");
 
 eduBtn.addEventListener("click", () => {
     edu.style.display = "flex";
     skills.style.display = "none";
-    project.style.display = "none";
+    exp.style.display = "none";
     activities.style.display = "none";
     certification.style.display = "none";
     home.style.display = "none";
 
     eduBtn.classList.add("active");
     skillsBtn.classList.remove("active");
-    projectBtn.classList.remove("active");
+    expBtn.classList.remove("active");
     activitiesBtn.classList.remove("active");
     certificationBtn.classList.remove("active");
 });
@@ -30,29 +30,29 @@ eduBtn.addEventListener("click", () => {
 skillsBtn.addEventListener("click", () => {
     edu.style.display = "none";
     skills.style.display = "flex";
-    project.style.display = "none";
+    exp.style.display = "none";
     activities.style.display = "none";
     certification.style.display = "none";
     home.style.display = "none";
 
     eduBtn.classList.remove("active");
     skillsBtn.classList.add("active");
-    projectBtn.classList.remove("active");
+    expBtn.classList.remove("active");
     activitiesBtn.classList.remove("active");
     certificationBtn.classList.remove("active");
 });
 
-projectBtn.addEventListener("click", () => {
+expBtn.addEventListener("click", () => {
     edu.style.display = "none";
     skills.style.display = "none";
-    project.style.display = "flex";
+    exp.style.display = "flex";
     activities.style.display = "none";
     certification.style.display = "none";
     home.style.display = "none";
 
     eduBtn.classList.remove("active");
     skillsBtn.classList.remove("active");
-    projectBtn.classList.add("active");
+    expBtn.classList.add("active");
     activitiesBtn.classList.remove("active");
     certificationBtn.classList.remove("active");
 });
@@ -60,14 +60,14 @@ projectBtn.addEventListener("click", () => {
 activitiesBtn.addEventListener("click", () => {
     edu.style.display = "none";
     skills.style.display = "none";
-    project.style.display = "none";
+    exp.style.display = "none";
     activities.style.display = "flex";
     certification.style.display = "none";
     home.style.display = "none";
 
     eduBtn.classList.remove("active");
     skillsBtn.classList.remove("active");
-    projectBtn.classList.remove("active");
+    expBtn.classList.remove("active");
     activitiesBtn.classList.add("active");
     certificationBtn.classList.remove("active");
 });
@@ -75,14 +75,14 @@ activitiesBtn.addEventListener("click", () => {
 certificationBtn.addEventListener("click", () => {
     edu.style.display = "none";
     skills.style.display = "none";
-    project.style.display = "none";
+    exp.style.display = "none";
     activities.style.display = "none";
     certification.style.display = "flex";
     home.style.display = "none";
 
     eduBtn.classList.remove("active");
     skillsBtn.classList.remove("active");
-    projectBtn.classList.remove("active");
+    expBtn.classList.remove("active");
     activitiesBtn.classList.remove("active");
     certificationBtn.classList.add("active");
 });
